7ce8607e8683e028633a2d82b86979627df2926f
gurpritsingh81 <56620498+gurpritsingh81@users.noreply.github.com>
2022-06-01 12:31:59 +0530

    Update Final.py

--- Final.py
+++ Final.py
@@ -173,8 +173,7 @@ if user_crypto:
         
         st.pyplot(fig)
         
-        linedata = pd.DataFrame([actual_price,prediction_prices]).T.rename(columns={0:'actual_price', 1:'prediction_prices'})
-        st.line_chart(linedata)
+        
        
         
         with st.expander("See explanation"):
